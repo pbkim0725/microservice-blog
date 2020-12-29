93e1c8e7ee961153467d1c006ea07b8eeee9a2cf
pbkim0725 <pbkim0725@gmail.com>
2020-12-29 09:31:01 +0900

    [Added] | Query Server Setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PostList from "./components/PostList";
+import PostCreate from "./components/PostCreate";
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1>Create Post</h1>
+			<PostCreate />
+			<hr />
+			<h1>Posts</h1>
+			<PostList />
+		</div>
+	);
+};
+
+export default App;
---
+++ client/src/components/CommentList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CommentList = ({ comments }) => {
+	const renderedComments = comments.map((comment) => {
+		return <li key={comment.id}>{comment.content}</li>;
+	});
+
+	return <div>{renderedComments}</div>;
+};
+
+export default CommentList;
